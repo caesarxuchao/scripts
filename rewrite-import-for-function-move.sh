91190efed74972a5d34535f6df1c09a831df6310
Chao Xu <xuchao@google.com>
2017-04-11 14:22:30 -0700

    update for move v1 helpers.go

--- rewrite-import-for-function-move.sh
+++ rewrite-import-for-function-move.sh
@@ -2,22 +2,44 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-old_pkg="api"
-new_pkg="scheme"
+old_pkg="v1"
+new_pkg="v1helper"
 
-old_import_path="\"k8s.io/kubernetes/pkg/api\""
-new_import_path="\"k8s.io/kubernetes/pkg/api/$new_pkg\""
+old_import_path="\"k8s.io/kubernetes/pkg/api/v1\""
+new_import_path="v1helper \"k8s.io/kubernetes/pkg/api/v1/helper\""
 
-old_import_path_client_go="\"k8s.io/client-go/pkg/api\""
-new_import_path_client_go="\"k8s.io/client-go/kubernetes/$new_pkg\""
+new_file="pkg/api/v1/$new_pkg/helpers.go"
 
-new_file="pkg/api/$new_pkg/register.go"
+functions="IsOpaqueIntResourceName
+OpaqueIntResourceName
+IsServiceIPSet
+IsServiceIPRequested
+AddToNodeAddresses
+LoadBalancerStatusEqual
+LoadBalancerStatusDeepCopy
+GetAccessModesAsString
+GetAccessModesFromString
+NodeSelectorRequirementsAsSelector
+AddOrUpdateTolerationInPod
+TolerationsTolerateTaint
+TolerationsTolerateTaintsWithFilter
+DeleteTaintsByKey
+DeleteTaint
+GetMatchingTolerations
+GetAvoidPodsFromNodeAnnotations
+SysctlsFromPodAnnotations
+SysctlsFromPodAnnotation
+PodAnnotationsFromSysctls
+AddOrUpdateTaint
+TaintExists
+RemoveTaint
+GetAffinityFromPodAnnotations
+GetPersistentVolumeClass
+GetPersistentVolumeClaimClass
+PersistentVolumeClaimHasClass"
 
-functions="GroupFactoryRegistry
-Registry
-Scheme
-Codecs
-ParameterCodec"
+
+
 
 for function in $functions; do
     echo "====================================="
@@ -31,6 +53,7 @@ for function in $functions; do
         sed -i "s/$old_pkg\.$function/$new_pkg.$function/g" $file
         if ! git grep -q "$new_pkg\.$function" $file; then
             echo "no real sed"
+            goimports -w $file
             continue
         fi
         if git grep -q "$new_import_path" $file; then
@@ -38,18 +61,13 @@ for function in $functions; do
             goimports -w $file
             continue
         fi
-        if git grep -q "$new_import_path_client_go" $file; then
-            echo "already update the import path"
-            goimports -w $file
-            continue
-        fi
-        sed -i "s|^\s*$old_import_path|$old_import_path\n$new_import_path|g" $file
-        sed -i "s|^\s*$old_import_path_client_go|$old_import_path_client_go\n$new_import_path_client_go|g" $file
+        sed -i "s|$old_import_path|$old_import_path\n$new_import_path|g" $file
         goimports -w $file
     done
 done
 
 # staging will be updated by other scripts
-#git checkout HEAD $K1/staging
+git checkout HEAD $K1/staging
 # exceptions
 #sed -i "/k8s.io\/kubernetes\/pkg\/api\"/d" $K1/pkg/api/v1/validation/validation.go
+sed -i "/k8s.io\/kubernetes\/pkg\/api\/ref\"/d" pkg/proxy/userspace/proxier.go
