7e4103cec454e2d21239f01edf12a6dbe90d1611
Chao Xu <xuchao@google.com>
2017-04-11 11:02:37 -0700

    update for move global vars

--- rewrite-import-for-function-move.sh
+++ rewrite-import-for-function-move.sh
@@ -3,25 +3,21 @@ set -o nounset
 set -o pipefail
 
 old_pkg="api"
-new_pkg="resource"
+new_pkg="scheme"
 
 old_import_path="\"k8s.io/kubernetes/pkg/api\""
 new_import_path="\"k8s.io/kubernetes/pkg/api/$new_pkg\""
 
-new_file="pkg/api/$new_pkg/resource_helpers.go"
+old_import_path_client_go="\"k8s.io/client-go/pkg/api\""
+new_import_path_client_go="\"k8s.io/client-go/kubernetes/$new_pkg\""
 
-functions="GetContainerStatus
-GetExistingContainerStatus
-IsPodAvailable
-IsPodReady
-IsPodReadyConditionTrue
-GetPodReadyCondition
-GetPodCondition
-GetNodeCondition
-UpdatePodCondition
-IsNodeReady
-PodRequestsAndLimits
-ExtractContainerResourceValue"
+new_file="pkg/api/$new_pkg/register.go"
+
+functions="GroupFactoryRegistry
+Registry
+Scheme
+Codecs
+ParameterCodec"
 
 for function in $functions; do
     echo "====================================="
@@ -39,14 +35,21 @@ for function in $functions; do
         fi
         if git grep -q "$new_import_path" $file; then
             echo "already update the import path"
+            goimports -w $file
             continue
         fi
-        sed -i "s|$old_import_path|$old_import_path\n$new_import_path|g" $file
-        gofmt -w $file
+        if git grep -q "$new_import_path_client_go" $file; then
+            echo "already update the import path"
+            goimports -w $file
+            continue
+        fi
+        sed -i "s|^\s*$old_import_path|$old_import_path\n$new_import_path|g" $file
+        sed -i "s|^\s*$old_import_path_client_go|$old_import_path_client_go\n$new_import_path_client_go|g" $file
+        goimports -w $file
     done
 done
 
 # staging will be updated by other scripts
-git checkout HEAD $K1/staging
+#git checkout HEAD $K1/staging
 # exceptions
 #sed -i "/k8s.io\/kubernetes\/pkg\/api\"/d" $K1/pkg/api/v1/validation/validation.go
